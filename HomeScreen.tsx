8468017efba3ae9dd01aa939895f6aa41a309cf0
joso7878 <joaoinacioclm@outlook.com>
2023-09-06 14:55:01 -0300

    backgrundImage

--- HomeScreen.tsx
+++ HomeScreen.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native';
 
-interface HomeScreenProps {
-  navigation: any;
-}
+// Defina o caminho da imagem em uma constante
+const backgroundImage = require('./assets/background.png');
 
-const HomeScreen: React.FC<HomeScreenProps> = ({ navigation }) => {
+const HomeScreen: React.FC<{ navigation: any }> = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Plantinhas</Text>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate('Login')}
+      {/* Container Superior com Imagem de Fundo */}
+      <ImageBackground
+        source={backgroundImage}
+        style={styles.upperContainer}
       >
-        <Text style={styles.buttonText}>Login</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate('Registration')}
-      >
-        <Text style={styles.buttonText}>Cadastro</Text>
-      </TouchableOpacity>
+        <Text style={styles.header}>Plantinhas</Text>
+      </ImageBackground>
+
+      {/* Container Inferior com Botões de Acesso */}
+      <View style={styles.lowerContainer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('Login')}
+        >
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('Registration')}
+        >
+          <Text style={styles.buttonText}>Cadastro</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -28,25 +37,40 @@ const HomeScreen: React.FC<HomeScreenProps> = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  upperContainer: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#FFFFFF', // primary background
+    width: 376, // Largura desejada
+    height: 442, // Altura desejada
+    top: -4, // Posição vertical desejada
   },
   header: {
     fontSize: 24,
     fontWeight: 'bold',
-    color: '#418B64', // primary color
+    color: '#418B64',
     marginBottom: 20,
   },
+  lowerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    padding: 20,
+  },
   button: {
-    backgroundColor: '#418B64', // primary color
+    backgroundColor: '#418B64',
     padding: 10,
     borderRadius: 5,
     marginBottom: 10,
-    width: 200,
+    width: 325,
+    height: 48,
   },
   buttonText: {
-    color: '#FFFFFF', // primary background
+    color: '#FFFFFF',
     fontSize: 18,
     textAlign: 'center',
   },
